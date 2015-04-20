7da9ae09a96391db2ee262e28186e30b13ebc793
Thomas Brüggemann <mail@thomasbrueggemann.com>
2015-04-20 16:45:29 +0200

    testcase

---
+++ test/test.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var translate 	= require("../translate");
+var fs 			= require("fs");
+var async   	= require("async");
+
+describe("Translation ", function () {
+
+	it("should not fail", function (done) {
+		true.should.equal(true);
+		return done();
+	});
+});
